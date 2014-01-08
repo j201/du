22b1c3b67122e99f47a33969f5fe973d44f2acae
unknown <alabach@sc079012.WTLDEV.NET>
2014-01-08 10:07:26 -0500

    Finish ts definitions and add tests

---
+++ tsdefs/du-tests.ts
@@ -0,0 +1,32 @@
+/// <reference path="./du.d.ts"/>
+
+var extendTest = du.getElementById('foo');
+var arr: number[] = du.toArray<number>({length: 1, 0: 99});
+var eachResult: number[] = du.each<number>({length: 1, 0: 99}, x => x);
+var id: HTMLElement = du.id("foo");
+var tags: HTMLCollection = du.tag("span");
+var subTags: HTMLCollection = du.tag(id, "div");
+var classes: HTMLCollection = du.className("bar");
+var subClasses: HTMLCollection = du.className(id, "bar");
+var query: HTMLElement = du.qs("#foo");
+var subQuery: HTMLElement = du.qs(id, "#foo");
+var queryAll: HTMLCollection = du.qsa(".bar");
+var subQueryAll: HTMLCollection = du.qsa(id, ".bar");
+var efn = e => console.log();
+du.event('load', efn);
+du.event(id, 'mouseover', efn);
+du.load(efn);
+du.click(efn);
+du.click(id, efn);
+du.rmEvent('keypress', efn);
+du.rmEvent(id, 'focus', efn);
+du.ready(efn);
+var text: Text = du.textNode('baz');
+var child: Text = du.setChild(id, text);
+var appendedText: Text = du.appendText(id, "baz");
+var prepended: Text = du.prepend(id, text);
+var removed: HTMLElement = du.remove(du.id('ohno'));
+var insertedAfter: Text = du.insertAfter(du.id('parent'), text, du.id('child'));
+var setText: Text = du.setText(id, 'baz');
+du.addClass(id, 'bar');
+du.rmClass(id, 'bar');
--- tsdefs/du.d.ts
+++ tsdefs/du.d.ts
@@ -1,15 +1,15 @@
-interface DuStatic extends Document { // TODO: clear not compatible
+interface DuStatic extends Document {
 	toArray<T>(arrayLike: {length: number}): T[];
 	each<T>(arrayLike: {length: number}, fn: (el: any, ...any)=>T): T[];
-	id(): HTMLElement;
+	id(id: string): HTMLElement;
 	tag(tagName: string): HTMLCollection;
 	tag(el: HTMLElement, tagName: string): HTMLCollection;
 	className(className: string): HTMLCollection;
 	className(el: HTMLElement, className: string): HTMLCollection;
 	qs(query: string): HTMLElement;
 	qs(el: HTMLElement, query: string): HTMLElement;
-	qsa(query: string): HTMLElement;
-	qsa(el: HTMLElement, query: string): HTMLElement;
+	qsa(query: string): HTMLCollection;
+	qsa(el: HTMLElement, query: string): HTMLCollection;
 	event(type: string, listener: (e: Event, ...any)=>any): void;
 	event(target: HTMLElement, type: string, listener: (e: Event, ...any)=>any): void;
 	load(listener: (e: Event, ...any)=>any): void;
@@ -18,6 +18,7 @@ interface DuStatic extends Document { // TODO: clear not compatible
 	rmEvent(type: string, listener: (e: Event, ...any)=>any): void;
 	rmEvent(target: HTMLElement, type: string, listener: (e: Event, ...any)=>any): void;
 	ready(listener: (e: Event, ...any)=>any): void;
+	clear(): void; // Doesn't actually exist on du, just to make TypeScript happy
 	clear(node: Node): Node[];
 	setChild<T extends Node>(node: Node, child: T): T;
 	textNode(text: string): Text;
@@ -26,8 +27,8 @@ interface DuStatic extends Document { // TODO: clear not compatible
 	remove<T extends Node>(node: T): T;
 	insertAfter<T extends Node>(node: Node, newNode: T, refNode: Node): T;
 	setText(node: Node, text: string): Text;
-	addClass(el: HTMLElement, ...string): Text;
-	rmClass(el: HTMLElement, ...string): Text;
+	addClass(el: HTMLElement, ...string): void;
+	rmClass(el: HTMLElement, ...string): void;
 }
 
 declare var du: DuStatic; 
